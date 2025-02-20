0bd855f6dd9cc3dfb0f20842139cd05ecd638718
YohannDelacroix <yohann.delacroix@hotmail.fr>
2025-02-20 20:26:01 +0100

    privateGallery page is now rendered in client side

--- src/app/shopping/privateGallery/[id]/page.tsx
+++ src/app/shopping/privateGallery/[id]/page.tsx
@@ -1,9 +1,13 @@
+"use client"
 import { parseParameter } from "next/dist/shared/lib/router/utils/route-regex";
 import PrivateGallery from "../../components/PrivateGallery";
+import { useSearchParams } from "next/navigation";
 
-export default function PrivateGalleryPage({ params }: { params: { id: string } }) {
-    
+export default function PrivateGalleryPage() {
+    const searchParams = useSearchParams();
+    const id = searchParams.get('id') ?? "";
+
     return (
-        <PrivateGallery id={params.id} />
+        <PrivateGallery id={id} />
     );
 }
