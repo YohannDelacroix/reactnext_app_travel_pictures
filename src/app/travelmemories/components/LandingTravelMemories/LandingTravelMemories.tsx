782eb0b0920202d4954ca8d0366a1c75bec1473f
YohannDelacroix <yohann.delacroix@hotmail.fr>
2025-02-23 11:05:48 +0100

    Added translations for landing page

--- src/app/travelmemories/components/LandingTravelMemories/LandingTravelMemories.tsx
+++ src/app/travelmemories/components/LandingTravelMemories/LandingTravelMemories.tsx
@@ -1,12 +1,24 @@
+"use client"
 import React from 'react'
 import TravelMemoriesAccessForm from './TravelMemoriesAccessForm'
+import { Trans, useTranslation } from 'react-i18next'
 
 const LandingTravelMemories = () => {
+    const { t } = useTranslation();
+
     return (
         <div className="flex flex-col items-center justify-center min-h-full text-center">
             <div className="max-w-xl w-full bg-white shadow-lg p-8 rounded-2xl text-center">
-                <h1 className="text-4xl text-[#a6c9e2] mb-4 font-extrabold">Get your travel photos</h1>
-                <p className="text-gray-600 mb-6">Have you just met one of our photographers in the street? Get access to the photos they took of you by entering your ID!</p>
+                <h1 className="text-4xl text-[#a6c9e2] mb-4 font-extrabold">
+                    <Trans i18nKey="landingPage.title"
+                        defaults="Get Your Travel Memories!"
+                    />
+                </h1>
+                <p className="text-gray-600 mb-6">
+                    <Trans i18nKey="landingPage.description"
+                        defaults="Did you just meet one of our photographers on the street? Gain access to the photos they took of you by entering the ID you received in your inbox!"
+                    />
+                </p>
                 <TravelMemoriesAccessForm />
             </div>
         </div>
--- src/app/travelmemories/components/LandingTravelMemories/TravelMemoriesAccessForm.tsx
+++ src/app/travelmemories/components/LandingTravelMemories/TravelMemoriesAccessForm.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react'
 import { PATH_PRIVATE_GALLERY } from '@/constants/paths';
 import { useDispatch } from 'react-redux';
 import { persistor, resetAll } from '../../store/store';
+import { Trans, useTranslation } from 'react-i18next';
 
 
 /**
@@ -11,6 +12,7 @@ import { persistor, resetAll } from '../../store/store';
  * @returns a JSX component to render the ID form to access the private gallery
  */
 const TravelMemoriesAccessForm = () => {
+    const { t } = useTranslation();
     const [galleryId, setGalleryId] = useState("");
     const router = useRouter();
     const dispatch = useDispatch();
@@ -38,13 +40,15 @@ const TravelMemoriesAccessForm = () => {
         <form onSubmit={handleSubmit} className="flex flex-col gap-4">
             <input className="w-full p-3 border border-gray-300 rounded-xl focus:outline-none focus:ring-2 focus:ring-[#a6c9e2] text-lg text-center"
                 type="text"
-                placeholder="Entrez l'ID de votre galerie"
+                placeholder={t("landingPage.inputPlaceholder")}
                 value={galleryId}
                 onChange={(e) => setGalleryId(e.target.value)}
             ></input>
             <button className="w-full p-3 bg-[#b4e1b9] text-white rounded-xl text-lg font-semibold hover:bg-[#98d4a9] transition-all"
                 type="submit">
-                Watch it !
+                <Trans i18nKey="landingPage.buttonText"
+                    defaults="View Now !"
+                />
             </button>
         </form>
     )
