784592638d4496b6d8286f3f6ec26b83ff110e52
YohannDelacroix <yohann.delacroix@hotmail.fr>
2025-02-25 18:53:47 +0100

    Fixed : Retrieving params in Server side privateGallery component is properly done and no longer generate errors when loading private Gallery page

--- src/app/travelmemories/privateGallery/[id]/page.tsx
+++ src/app/travelmemories/privateGallery/[id]/page.tsx
@@ -1,7 +1,7 @@
 import PrivateGallery from "../../components/PrivateGallery";
+type Params = Promise<{ id: string }>;
 
-
-export default async function PrivateGalleryPage({ params }: { params: { id: string } }) {
+export default async function PrivateGalleryPage({ params }: { params: Params }) {
     const { id } = await params;
     return (
         <PrivateGallery id={id} />
